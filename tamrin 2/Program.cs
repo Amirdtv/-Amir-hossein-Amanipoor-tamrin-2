1d01bda8fbaced9f75e8b3f6320fab9c9c5f3b7f
Amir Hossein Amanipoor <76044261+Amirdtv@users.noreply.github.com>
2023-02-24 10:33:13 +0330

    The second

---
+++ tamrin 2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tamrin_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("2 adad vared konin : ");
+            int x;
+            x = Convert.ToInt32(Console.ReadLine());
+            int y;
+            y = Convert.ToInt32(Console.ReadLine());
+            int z;
+            z = x + y;
+            System.Console.WriteLine(z);
+            System.Console.ReadKey();
+        }
+    }
+}
